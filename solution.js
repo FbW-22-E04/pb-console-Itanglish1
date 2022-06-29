1da36e5bc799bb441e4ce21b2cd0311b91c0015f
chris <chrishethertonweb@gmail.com>
2022-06-29 13:00:39 +0200

    1, 2 and 3 instructions done

--- solution.js
+++ solution.js
@@ -1,2 +1,9 @@
 // write your code in this
 console.log("This is an exercise in console logging");
+
+let exercise = "This is the value.";
+console.log(exercise);
+
+let firstName = "Chris";
+let lastName = "H";
+console.log(firstName, lastName);
